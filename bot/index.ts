e9608bf4f57fd5786408d171912327089c2cf810
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2023-05-10 02:01:12 +0900

    とりあえずオウム返し実装

--- bot/index.ts
+++ bot/index.ts
@@ -1,9 +1,38 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-import Express from "express";
-const app = Express();
-
-app.listen(3000, () => {
-  console.info("Slack Assistant Bot is Running...");
+import { App, ExpressReceiver } from "@slack/bolt";
+const expressReceiver = new ExpressReceiver({
+  signingSecret: process.env.SLACK_SIGN_SECRET ?? "",
+  processBeforeResponse: true,
 });
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGN_SECRET ?? "",
+  receiver: expressReceiver,
+  processBeforeResponse: true,
+  endpoints: "/slack/events",
+});
+
+app.event("app_mention", async ({ event, say }) => {
+  const msg = event.text.replace(/<@\w+>/g, "");
+  await say({
+    text: `<@${event.user}>` + msg,
+    thread_ts: event.thread_ts || event.ts,
+  })
+});
+
+import * as AwsServerlessExpress from "aws-serverless-express";
+import { APIGatewayProxyEvent, Context } from "aws-lambda";
+const server = AwsServerlessExpress.createServer(expressReceiver.app);
+
+export const lambdaHandler = (event: APIGatewayProxyEvent, context: Context): void => {
+  AwsServerlessExpress.proxy(server, event, context);
+}
+
+if (process.env.SERVER_RUNNING_MODE === "Local") {
+  (async () => {
+    await app.start(3000);
+    console.info("Assistant Bot Server Running...");
+  })();
+}
